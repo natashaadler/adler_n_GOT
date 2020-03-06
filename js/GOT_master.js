61ddb519de3795b5e31f81915054329dfe905e93
Natasha Adler <natashaadler@Natashas-MBP.fc.ca>
2020-03-06 10:23:28 -0500

    adding light box and dynamics

--- js/GOT_master.js
+++ js/GOT_master.js
@@ -14,10 +14,24 @@ let sigilButtons = document.querySelectorAll('.sigilContainer'),
   function showLightBox() {
   // pop open a light box here and show some content
   //start with a video
+  //debugger;
+
+  //need to get the class name from element so we can match the video source
+  // $ lets you do it right in line and return back a value
+  //split - splits the name into two pieces as they all start with House- name of video
+  let targetName = this.className.split(" ")[1] //this will strip out the house className
+  //makes it upper case to match video clip
+  let targetSource = targetName.charAt(0).toUpperCase() + targetName.slice(1);
+
+  let newVideoSource = `video/House-${targetSource}.mp4`;
+  //debugger;
+
 
     lightBox.classList.add("show-lightBox");
 
 //plays video as soon as you open the view port
+    gotVideo.src = newVideoSource
+    gotVideo.load();
     gotVideo.play();
   }
 
